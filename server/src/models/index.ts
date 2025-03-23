329eedf24aacdbd26cf3f5cd08702b33ec094ccb
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-23 10:08:10 +0000

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -12,7 +12,7 @@ console.log("DB_User", process.env.DB_USER);
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: 'dpg-cvfhpotsvqrc73d1qahg-a',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
